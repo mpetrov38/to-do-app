3b84ffea978240c8914b0eeb2bb7486e75df9d63
Milen <petrovmilen38@gmail.com>
2023-06-10 15:47:08 +0300

    initial structure cleaned up the starting project

---
+++ src/components/Main/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Main({ children }){
+  return (
+    <div className="Main">
+      {props.children}
+    </div>
+  )
+}
+
+export default Main;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidebar({ children }){
+  return (
+    <div className="Main">
+      {props.children}
+    </div>
+  )
+}
+
+export default Sidebar;
